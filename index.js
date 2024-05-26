2996d3490222411f072f695ea6251e23ec11852d
Aashrayakarki <aashrayakarki7@gmail.com>
2024-05-26 13:32:23 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is Running on port ${PORT}!`)
+})
+
+app.get('/', (req, res) => {
+
+    res.send('Hello World!');
+
+});
