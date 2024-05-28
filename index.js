86538220fd595892c2a99bd1ffbdc2501e7b2832
Aashrayakarki <aashrayakarki7@gmail.com>
2024-05-28 21:47:17 +0545

    made routes to post on postman

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const express = require('express');
 const connectDatabase = require('./database/database.js')
 const dotenv = require('dotenv');
 
-
-
 const app = express();
 
-const PORT = 5000;
+app.use(express.json())
 
 dotenv.config()
 
 connectDatabase()
 
+const PORT = process.env.PORT;
+
+app.get('/test', (req, res) => {
+
+    res.send('Hello World!')
+
+})
+
+app.use('/api/user', require('./routes/userRoutes.js'))
+
 app.listen(PORT, ()=>{
     console.log(`Server is Running on port ${PORT}!`)
-});
+})
