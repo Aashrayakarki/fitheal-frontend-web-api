67215861fcaa2a3eafd1ec230ec546e5c4b52973
Aashrayakarki <aashrayakarki7@gmail.com>
2024-05-29 13:02:26 +0545

    deleted wrong files

--- routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const userController = require('../controllers/userController.js');
-
-router.post('/register', userController.createUser);
-
-module.exports = router;
