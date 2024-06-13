184122b61f723e3a94c71043d6631d931bdc19a8
Aashrayakarki <aashrayakarki7@gmail.com>
2024-06-13 16:55:43 +0545

    Created Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
 
 const Navbar = () => {
-    //get user data from local storage
-    const user = JSON.parse(localStorage.getItem('userData'))
-
     return (
         <>
-            <div className='container'>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Fit<span className='text-danger'>Heal</span></a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Link</a>
-                                </li>
-                            </ul>
-                            <form className="d-flex" role="search">
-                                {
-                                    user ? (<>
-                                        <div class="dropdown">
-                                            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                Welcome, test!
-                                            </a>
-
-                                            <ul class="dropdown-menu">
-                                                <li><a class="dropdown-item" href="#">Profile</a></li>
-                                                <li><a class="dropdown-item" href="#">Settings</a></li>
-                                                <li><a class="dropdown-item" href="#">Log Out</a></li>
-                                            </ul>
-                                        </div>
-                                    </>)
-                                        : (<>
-                                            <Link to={'/login'} className="btn btn-primary" type="Submit">Login</Link>
-                                            <Link to={'/register'} className="btn btn-success ms-2" type="Submit">Register</Link></>)
-                                }
-                            </form>
-                        </div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand ms-5 me-5" href="#">FitHeal</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav mx-auto">
+                            <li className="nav-item me-5">
+                                <a className="nav-link" href="#">Home</a>
+                            </li>
+                            <li className="nav-item me-5">
+                                <a className="nav-link" href="#">Exercises</a>
+                            </li>
+                            <li className="nav-item me-5">
+                                <a className="nav-link" href="#">Meal Plans</a>
+                            </li>
+                            <li className="nav-item me-5">
+                                <a className="nav-link" href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav ms-5 me-5">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Welcome
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
                     </div>
-                </nav>
-            </div>
+                </div>
+            </nav>
         </>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
