4fb614075d50723155294d037400bac479b7e9c6
Aashrayakarki <aashrayakarki7@gmail.com>
2024-07-20 17:08:21 +0545

    added exercise video inside a modal in exercise card

--- src/components/exercise_card/ExerciseCard.jsx
+++ src/components/exercise_card/ExerciseCard.jsx
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExerciseCard.css';
 
 const ExerciseCard = ({ exerciseInformation, color }) => {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+
     return (
-        <div className="card custom-card" style={{ width: '18rem' }}>
-            <span style={{ backgroundColor: color }} className='badge position-absolute top-0'>{exerciseInformation.exerciseLevel}</span>
-            <img 
-                src={`http://localhost:5000/products/${exerciseInformation.exerciseThumbnail}`} 
-                className="card-img-top custom-card-img" 
-                alt={exerciseInformation.exerciseName}
-            />
-            <div className="card-body">
-                <div className='d-flex justify-content-between'>
-                    <h5 className="card-title">{exerciseInformation.exerciseName}</h5>
-                    <h5 className="card-title text-danger">{exerciseInformation.exerciseCalories} cal</h5>
+        <div>
+            <div className="card custom-card" style={{ width: '18rem' }}>
+                <span style={{ backgroundColor: color }} className='badge position-absolute top-0'>
+                    {exerciseInformation.exerciseLevel}
+                </span>
+                <img 
+                    src={`http://localhost:5000/products/${exerciseInformation.exerciseThumbnail}`} 
+                    className="card-img-top custom-card-img" 
+                    alt={exerciseInformation.exerciseName}
+                />
+                <div className="card-body">
+                    <div className='d-flex justify-content-between'>
+                        <h5 className="card-title">{exerciseInformation.exerciseName}</h5>
+                        <h5 className="card-title text-danger">{exerciseInformation.exerciseCalories} cal</h5>
+                    </div>
+                    <p className="card-text">{exerciseInformation.exerciseTime} minutes</p>
+                    <button className="btn btn-outline-dark w-100" onClick={handleShowModal}>
+                        Details
+                    </button>
                 </div>
-                <p className="card-text">{exerciseInformation.exerciseTime.slice(0, 40)} minutes</p>
-                <a href="#" className="btn btn-outline-dark w-100">Details</a>
             </div>
+
+            {/* Modal */}
+            {showModal && (
+                <div className="modal show d-block" tabIndex="-1" role="dialog">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">{exerciseInformation.exerciseName} Video</h5>
+                            </div>
+                            <div className="modal-body">
+                                <video width="100%" controls>
+                                    <source src={`http://localhost:5000/products/${exerciseInformation.exerciseVideo}`} type="video/mp4" />
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" onClick={handleCloseModal}>
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
